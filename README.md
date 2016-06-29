937b51ffa83fa21707339e5de46cb8116e92e9d7
gbk <ck0123456@gmail.com>
2016-06-29 23:40:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# nowa-dep
+
+[![NPM version](https://img.shields.io/npm/v/nowa-dep.svg?style=flat)](https://npmjs.org/package/nowa-dep)
+
+dependencies installer
+
+---
+
+## Feature
+
+- dependencies installer
+
+## Install
+
+```bash
+$ npm i nowa -g
+$ nowa install dep
+```
+
+## Usage
+
+```bash
+$ nowa dep --help
+```
---
+++ src/index.js
@@ -0,0 +1,70 @@
+/*
+* @Author: gbk <ck0123456@gmail.com>
+* @Date:   2016-04-21 17:34:00
+* @Last Modified by:   gbk
+* @Last Modified time: 2016-06-29 23:39:55
+*/
+
+'use strict';
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var pkg = require('../package.json');
+
+// plugin defination
+module.exports = {
+
+  command: 'dep <command>',
+
+  description: pkg.description,
+
+  options: [
+    [ '    --npm <npm>', 'which npm to use' ]
+  ],
+
+  action: function(command, options) {
+    switch (command) {
+      case 'install':
+      case 'update':
+        try {
+
+          // filter out nowa & nowa plugins from dependencies
+          var pkgPath = 'package.json';
+          var pkgText = fs.readFileSync(pkgPath, 'utf-8');
+          var pkgJson = JSON.parse(pkgText);
+          filterOutNowa(pkgJson.dependencies);
+          filterOutNowa(pkgJson.devDependencies);
+          fs.writeFileSync(pkgPath, JSON.stringify(pkgJson));
+
+          // run npm command
+          var cmd = (options.npm || 'npm') + ' ' + command;
+          console.log('Running ' + cmd);
+          exec(cmd, function(err, stdout, stderr) {
+
+            // revert package.json
+            fs.writeFileSync(pkgPath, pkgText);
+            console.log(err || stderr || stdout);
+            if (!err) {
+              console.log('Done.');
+            }
+          });
+        } catch(e) {
+          console.error(e);
+        }
+        break;
+      default:
+        console.error('Unknow command ' + command);
+    }
+  }
+};
+
+// filter out nowa & nowa plugins from dependencies
+function filterOutNowa(dependencies) {
+  dependencies = dependencies || {};
+  for (var key in dependencies) {
+    if (/^nowa($|\-)/.test(key)) {
+      delete dependencies[key];
+    }
+  }
+}
